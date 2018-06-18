f289974be6ef7dd91b1b89df02be0b5edc175a2c
Adele <34361956+Tellarra@users.noreply.github.com>
2018-06-18 21:55:40 +1000

    fixed merge
    fixed merge

--- DAL/db_functions.php
+++ DAL/db_functions.php
@@ -3,7 +3,6 @@
 // Library of Database Functions
 
 // Database Connection Variables
-<<<<<<< HEAD
 $localhost = "localhost";
 $user = "root";
 $password = "root";
@@ -32,41 +31,11 @@ function connect() {
 }
 
 //Selecting all records from a table
-function readQuery( $table ) {
-=======
-$localhost="localhost";
-$user="root";
-$password="root";
-$db="dalton";
-$dsn="mysql:host=$localhost;dbname=$db;";
-
-// Declare Global Variables
-$dbConnection=NULL;
-$stmt=NULL;
-$numRecords=NULL;
-
-// Establish MySQL Connection
-function connect() {
-  global $user, $password, $dsn, $dbConnection; // Required to access global Variables
-
-  try {
-    // Create a PDO connection with the configuration data
-    $dbConnection = new PDO($dsn, $user, $password);
-  }
-  catch(PDOException $error) {
-    // Display error message if applicable
-    echo "An Error occured: ".$error->getMessage();
-  }
-}
-
-//Selecting all records from a table
 function readQuery($table) {
->>>>>>> 385a2b4dc8985edca72b4e404afd169067cf4b6a
 	global $numRecords, $dbConnection, $stmt;
 	//Connect to DB
 	connect();
 	//Create string
-<<<<<<< HEAD
 	$sqlStr = "SELECT * FROM " . $table;
 
 	//Run Query
@@ -429,25 +398,3 @@ function readQuerySingle( $table, $column, $colValue, $colType ) {
 	$dbConnection = NULL;
 }
 ?>
-=======
-	$sqlStr = "SELECT * FROM ".$table;
-	
-	//Run Query
-	try {
-		$stmt = $dbConnection->query($sqlStr);
-		//If this is not working do 
-		if($stmt === false) { //That
-			die("Error executing the query: $sqlStr");
-		}
-	} catch(PDOException $error) {
-		echo "Error executing query: ".$error->getMessage();
-	}
-	
-	//Number of records that has been returned
-	$numRecords = $stmt->rowcount();
-	
-	//Close DB connection
-	$dbConnection = NULL;
-}
-?>
->>>>>>> 385a2b4dc8985edca72b4e404afd169067cf4b6a
--- html/loginPage.php
+++ html/loginPage.php
@@ -1,16 +1,16 @@
 <!doctype html>
 <?php
-	//include some required files
-	require_once("../DAL/db_functions.php");
-	require_once("../BLL/validate_data.php");
+//include some required files
+require_once( "../DAL/db_functions.php" );
+require_once( "../BLL/validate_data.php" );
 
-	$userName = $passwordUser = "";
-	$username_err = $password_err = "";
-	
-	if(isset($_POST['submit']) ) {
-		console_log("submitted");
-		validateLogin("login");
-	}
+$userName = $passwordUser = "";
+$username_err = $password_err = "";
+
+if ( isset( $_POST[ 'submit' ] ) ) {
+	console_log( "submitted" );
+	validateLogin( "login" );
+}
 ?>
 <html>
 
@@ -23,6 +23,7 @@
 		body {
 			font: 14px sans-serif;
 		}
+		
 		.wrapper {
 			width: 350px;
 			padding: 20px;
@@ -36,7 +37,7 @@
 			<a class="logo" title="Dalton It" href="../index.php"><span>Dalton It</span></a>
 		</header>
 		<div class="grid">
-		<?php 
+			<?php 
 
 	//create table project details
 	echo"<div class='wrapper'>";
@@ -60,27 +61,52 @@
 	echo "<p>Already have an account? <a href='signUp.php'>Login here</a>.</p>";
 	echo "</form></div></div>";
 ?>
-	</div>
-		<nav>
-			<ul>
-				<li>
-					<a title="About Us" href="aboutUs.html">About Us</a>
-				</li>
-				<li>
-					<a title="Contact Us" href="contactUs.html">Contact Us</a>
-				</li>
-				<li hidden="true">
-					<a title="View Consultant Records" href="../php/viewConsultant.php">View Consultant</a>
-				</li>
-				<li hidden="true">
-					<a title="View Project Records" href="../php/viewProject.php">View Project</a>
-				</li>
-				<li hidden="true">
-					<a title="View Project Consultant Record" href="../php/viewProjectStaff.php">View Project Consultant</a>
-				</li>
-			</ul>
-		</nav>
-	</div>
+			<nav>
+				<ul>
+					<li>
+						<a title="About Us" href="html/aboutUs.html">About Us</a>
+					</li>
+					<li>
+						<a title="Contact Us" href="html/contactUs.html">Contact Us</a>
+					</li>
+					<?php
+					//include some required files
+					require_once( "../DAL/db_functions.php" );
+					require_once( "../BLL/validate_data.php" );
+
+					global $_COOKIE;
+
+					if ( !isset( $_COOKIE[ 'Dalton_IT_auth' ] ) ) {
+						echo "<li>";
+						echo "<a title='Login' href='html/loginPage.php'>Login</a>";
+						echo "</li>";
+						//echo "Cookie named '" . $cookie_name . "' is not set!";
+					} else {
+						//echo "Cookie '" . $cookie_name . "' is set!<br>";
+						//echo "Value is: " . $_COOKIE['Dalton_IT_auth'];
+						echo "<li>";
+						echo "<a title='View Consultant Records' href='../php/viewConsultant.php'>View Consultant</a>";
+						echo "</li>";
+
+						echo "<li>";
+						echo "<a title='View Project Records' href='../php/viewProject.php'>View Project</a>";
+						echo "</li>";
+
+						echo "<li>";
+						echo "<a title='View Project Consultant Record' href='../php/viewProjectStaff.php'>View Project Consultant</a>";
+						echo "</li>";
+					}
+					?>
+				</ul>
+			</nav>
+
+			<footer>
+				&copy; Dalton It
+				<div class="content">
+					<a title="Privacy Policy" href="#">Privacy Policy</a>
+					<a title="Terms of Service" href="#">Terms of Service</a>
+				</div>
+			</footer>
 </body>
 
 </html>
